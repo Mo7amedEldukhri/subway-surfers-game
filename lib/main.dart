476d29a829369dcac0ba1a7695b92c12c889ffc4
Mo7amedEldukhri <elbossmax199616@gmail.com>
2026-05-25 08:04:53 -0700

    Add: main.dart entry point

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+import 'game/subway_surfers_game.dart';
+
+void main() {
+  runApp(const SubwaySurfersApp());
+}
+
+class SubwaySurfersApp extends StatelessWidget {
+  const SubwaySurfersApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Subway Surfers',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: GameWidget(game: SubwaySurfersGame()),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
